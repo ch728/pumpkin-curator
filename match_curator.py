6598ad1db308ab0991e6810cad5b6918b5a2e9b4
Chris Hernandez <christopher.hernandez@unh.edu>
2026-04-03 17:15:13 -0400

    test

--- match_curator.py
+++ match_curator.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
-from streamlit_aggrid import AgGrid, GridOptionsBuilder, DataReturnMode, GridUpdateMode
+from streamlit-aggrid import AgGrid, GridOptionsBuilder, DataReturnMode, GridUpdateMode
 
 
 st.set_page_config(page_title="Pumpkin Match Curator", layout="wide")
